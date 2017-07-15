a7e2c78871c24e457cd89463a55d97fa89c54e54
ichibiko <ichibiko>
2017-07-16 01:08:54 +0900

    create src/template-head.sh

--- src/main.sh
+++ src/main.sh
@@ -34,28 +34,7 @@ if [ -n "$OUTPUT_FILEPATH" ]; then
     exec > $OUTPUT_FILEPATH
 fi
 
-cat <<\EOF
-#!/bin/sh
-
-if [ -z "$WORKING_DIR" ]; then
-    if [ -z "$UID" ]; then
-        UID=$(id -u)
-    fi
-    if [ -d /run/user/$UID ]; then
-        export WORKING_DIR=$(mktemp -d /run/user/$UID/ichipack-XXXXXXXX)
-    elif [ -d /dev/shm ]; then
-        export WORKING_DIR=$(mktemp -d /dev/shm/ichipack-XXXXXXXX)
-    else
-        export WORKING_DIR=$(mktemp -d)
-    fi
-else
-    export WORKING_DIR=$WORKING_DIR/.ttr/child-$$
-    mkdir $WORKING_DIR
-fi
-
-trap "rm -rf $WORKING_DIR" EXIT
-
-EOF
+cat $WORKING_DIR/src/template-head.sh
 
 (
     cd $TARGET_DIR
