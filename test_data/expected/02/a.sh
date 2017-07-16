96b6bde2222ef29652dcdc95919aedd1096df52d
ichibiko <ichibiko>
2017-07-17 00:22:00 +0900

    simplify rule of WORKING_DIR

--- test_data/expected/02/a.sh
+++ test_data/expected/02/a.sh
@@ -2,20 +2,15 @@
 
 ####################################################################################################
 
-if [ -z "$WORKING_DIR" ]; then
-    if [ -z "$UID" ]; then
-        UID=$(id -u)
-    fi
-    if [ -d /run/user/$UID ]; then
-        export WORKING_DIR=$(mktemp -d /run/user/$UID/ichipack-XXXXXXXX)
-    elif [ -d /dev/shm ]; then
-        export WORKING_DIR=$(mktemp -d /dev/shm/ichipack-XXXXXXXX)
-    else
-        export WORKING_DIR=$(mktemp -d)
-    fi
+if [ -z "$UID" ]; then
+    UID=$(id -u)
+fi
+if [ -d /run/user/$UID ]; then
+    export WORKING_DIR=$(mktemp -d /run/user/$UID/ichipack-XXXXXXXX)
+elif [ -d /dev/shm ]; then
+    export WORKING_DIR=$(mktemp -d /dev/shm/ichipack-XXXXXXXX)
 else
-    export WORKING_DIR=$WORKING_DIR/.ttr/child-$$
-    mkdir $WORKING_DIR
+    export WORKING_DIR=$(mktemp -d)
 fi
 
 trap "rm -rf $WORKING_DIR" EXIT
